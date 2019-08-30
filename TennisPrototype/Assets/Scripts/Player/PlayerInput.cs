12d8f39733120c5d4bcf5bc67d660e5e4d886fb2
Naani <snstheja@gmail.com>
2019-08-29 23:44:10 -0800

    Added Basic Movement

---
+++ TennisPrototype/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    private FloatVariableSO _HorizontalAxis;
+    [SerializeField]
+    private FloatVariableSO _VerticalAxis;
+    void Update()
+    {
+        ReadInput();
+    }
+
+    private void ReadInput()
+    {
+        _HorizontalAxis.Value = Input.GetAxis("Horizontal");
+        _VerticalAxis.Value = Input.GetAxis("Vertical");
+    }
+}
---
+++ TennisPrototype/Assets/Scripts/ScriptableObjects/FloatVariableSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="SO",menuName = "ScriptableObj/FloatVariable" )]
+public class FloatVariableSO : ScriptableObject
+{
+    public float Value;
+}
