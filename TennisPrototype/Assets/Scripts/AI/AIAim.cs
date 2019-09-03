62a2b708b7613b3bcdcfb1f8c7387041768e03c8
Naani <snstheja@gmail.com>
2019-09-02 22:51:04 -0800

    Added simple enemy to play rallies and test out ball physics

---
+++ TennisPrototype/Assets/Scripts/AI/AIAim.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAim : MonoBehaviour
+{
+    [SerializeField]
+    private TransformSO _EnemyAimTargetTransformSo;
+
+    void Start()
+    {
+        _EnemyAimTargetTransformSo.Value = this.transform;
+    }
+
+ 
+}
---
+++ TennisPrototype/Assets/Scripts/AI/AIPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private TransformSO _BallTransformSo;
+    [SerializeField]
+    private float _AISpeed;
+    private Vector3 _TargetPosition;
+
+    private void Start()
+    {
+        _TargetPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AIMove();
+    }
+   private void AIMove()
+    {
+        _TargetPosition.z = _BallTransformSo.Value.position.z;
+        transform.position = Vector3.MoveTowards(transform.position, _TargetPosition, _AISpeed * Time.deltaTime);
+    }
+}
